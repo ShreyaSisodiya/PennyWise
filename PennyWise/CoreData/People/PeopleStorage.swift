030915fa3ff06e2ae6b509d79a86c035b6c97f6d
ShreyaSisodiya <sisodiya.s@northeastern.edu>
2024-10-30 18:41:20 -0400

    Added People Storage file

--- PennyWise/CoreData/People/PeopleStorage.swift
+++ PennyWise/CoreData/People/PeopleStorage.swift
@@ -5,3 +5,40 @@
 //  Created by Shreya Sisodiya on 10/30/24.
 //
 
+import Foundation
+import Combine
+import CoreData
+
+class PeopleStorage: NSObject, ObservableObject {
+    
+    var people = CurrentValueSubject<[People], Never>([])
+    private let peopleFetchCotroller: NSFetchedResultsController<People>
+    let viewContext = PersistenceController.shared.container.viewContext
+    
+    static let shared: PeopleStorage = PeopleStorage()
+    
+    private override init() {
+        let fetchRequest: NSFetchRequest<People> = People.fetchRequest()
+        fetchRequest.sortDescriptors = []
+        peopleFetchCotroller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        super.init()
+        
+        peopleFetchCotroller.delegate = self
+        
+        do{
+            try peopleFetchCotroller.performFetch()
+            people.value = peopleFetchCotroller.fetchedObjects ?? []
+        } catch{
+            NSLog("Error: Could not fetch objects")
+        }
+    }
+}
+
+extension PeopleStorage: NSFetchedResultsControllerDelegate {
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        guard let people = controller.fetchedObjects as? [People] else { return }
+        self.people.value = people
+    }
+}
