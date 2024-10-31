5a3968330f18ad77d183f65708d27190d6209500
ShreyaSisodiya <sisodiya.s@northeastern.edu>
2024-10-30 20:16:29 -0400

    Added constants for Trips Model

---
+++ PennyWise/Trips/TripsModel.swift
@@ -0,0 +1,15 @@
+//
+//  TripsModel.swift
+//  PennyWise
+//
+//  Created by Shreya Sisodiya on 10/30/24.
+//
+
+import Foundation
+import CoreGraphics
+
+// Constants
+let tripsNavigationTitle: String = "Trips"
+let plusImage: String = "plus"
+let tripsVStackSpacing: CGFloat = 10.0
+let tripsHStackSpacing: CGFloat = 75.0
