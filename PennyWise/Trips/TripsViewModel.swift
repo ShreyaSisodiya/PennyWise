de533566cbbd986d7921393b6ceec7e047275f32
ShreyaSisodiya <sisodiya.s@northeastern.edu>
2024-10-30 20:21:58 -0400

    Defined viewModel for Trips

--- PennyWise/Trips/TripsViewModel.swift
+++ PennyWise/Trips/TripsViewModel.swift
@@ -5,3 +5,16 @@
 //  Created by Shreya Sisodiya on 10/30/24.
 //
 
+import Foundation
+import Combine
+
+class TripsViewModel: ObservableObject{
+    @Published var trips: [Trips] = []
+    private var cancellable: AnyCancellable?
+    
+    var tripsPublisher: AnyPublisher<[Trips], Never> = TripsStorage.shared.trips.eraseToAnyPublisher()
+    
+    init(){
+        cancellable = tripsPublisher.sink { trips in self.trips = trips}
+    }
+}
