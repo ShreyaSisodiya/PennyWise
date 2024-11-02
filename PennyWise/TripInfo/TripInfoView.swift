9cd3bf58b7b9080532e58c44903bec56fa3766a9
ShreyaSisodiya <sisodiya.s@northeastern.edu>
2024-11-02 14:46:09 -0400

    defined model and view to display trip info. navigation to settleup and expenses view remains

--- PennyWise/TripInfo/TripInfoView.swift
+++ PennyWise/TripInfo/TripInfoView.swift
@@ -8,11 +8,65 @@
 import SwiftUI
 
 struct TripInfoView: View {
+    
+    @StateObject private var tripInfoViewModel: TripInfoViewModel
+    let currentTrip: Trips
+    
+    init(currentTrip: Trips, tripInfoViewModel: TripInfoViewModel){
+        self.currentTrip = currentTrip
+        self._tripInfoViewModel = StateObject(wrappedValue: tripInfoViewModel)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                Form{
+                    Section(header: Text(tripPeopleHeader)){
+                        List{
+                            ForEach(tripInfoViewModel.peopleNames, id: \.self){ name in
+                                Text(name)
+                            }
+                        }
+                    }
+                    
+                    Section(header: Text(addPersonHeader)){
+                        Picker(peopleNamePickerText, selection: $tripInfoViewModel.personChosen){
+                            ForEach(tripInfoViewModel.people, id: \.self){ item in
+                                Text(item.wrappedName)
+                            }
+                        }
+                        Button(action: tripInfoViewModel.addPerson){
+                            RoundedRectangle(cornerRadius: buttonCornerRadius)
+                                .frame(height: buttonFrameHeight)
+                                .overlay(Text(addPersonButtonText).foregroundColor(.white))
+                        }.padding()
+                    }
+                    
+                    Section(header: Text(addNewPersonHeader)){
+                        TextField(newNameText, text: $tripInfoViewModel.newPersonName)
+                        TextField(newEmailText, text: $tripInfoViewModel.newPersonEmail)
+                        Button(action: tripInfoViewModel.addNewPerson){
+                            RoundedRectangle(cornerRadius: buttonCornerRadius)
+                                .frame(height: buttonFrameHeight)
+                                .overlay(Text(addNewPersonButtonText).foregroundColor(.white))
+                        }.padding()
+                            .disabled(!tripInfoViewModel.newPersonIsValid)
+                    }
+                    
+                    Button(action: {tripInfoViewModel.confirmChanges(currentTrip: currentTrip)}){
+                        RoundedRectangle(cornerRadius: buttonCornerRadius)
+                            .frame(height: buttonFrameHeight)
+                            .overlay(Text(confirmChangesButtonText).foregroundColor(.white))
+                    }.padding()
+                }
+            }
+            
+            //add nav links to xpenses view and settleup view
+            .navigationTitle(Text(currentTrip.wrappedName))
+        }
     }
 }
 
-#Preview {
-    TripInfoView()
-}
+//#Preview {
+//    TripInfoView()
+//}
